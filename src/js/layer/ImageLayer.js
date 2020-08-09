98c4f8b893c8ae8b527aba9df34bcdd99515832e
Xuew <1914226339@qq.com>
2020-08-09 15:50:48 +0800

    2020/8/9 -1

--- src/js/layer/ImageLayer.js
+++ src/js/layer/ImageLayer.js
@@ -526,6 +526,60 @@
 			});
 			return true;
 		},
+		pancil:function(imageData,value = 1){ //铅笔画
+			/*
+				利用图像边缘检测算法实现
+				Sobel算子：
+				kernelX=[[-1,0,1],   kernelY=[[-1,-2,-1], 
+				 		 [-2,0,2],			  [ 0, 0, 0],
+				 		 [-1,0,1]]		      [ 1, 2, 1]]
+				Scharr算子：
+				kernelX=[[-3 ,0,3 ],   kernelY=[[-3,-10,-3], 
+				 		 [-10,0,10],		  	[ 0, 0 , 0],
+				 		 [-3 ,0,3 ]]		  	[ 3, 10, 3]]
+				Gx = kerelX※src, Gy = kerelY※src // 卷积
+				G = sqrt(Gx^2+Gy^2)
+				提高效率：G = |Gx|+|Gy|
+			*/
+			let {data,width,height} = imageData;
+			let kernelX,kernelY;
+			if(value === 1){					// Sobel算子  [x,y,value]
+				kernelX = [[-1,-1,-1],[-1,1,1],[0,-1,-2],[0,1,2],[1,-1,-1],[1,1,1]];
+				kernelY = [[-1,-1,-1],[-1,0,-2],[-1,1,-1],[1,-1,1],[1,0,2],[1,1,1]];
+			}else{								// Scharr算子
+				kernelX = [[-1,-1,-3],[-1,1,3],[0,-1,-10],[0,1,10],[1,-1,-3],[1,1,3]];
+				kernelY = [[-1,-1,-3],[-1,0,-10],[-1,1,-3],[1,-1,3],[1,0,10],[1,1,3]];
+			}
+			let tmpData = new Uint8ClampedArray(height*width*4);
+			let index,Gx,Gy,G;
+			function calc(i,j,k,kernel){ 
+				let x,y,index;
+				x = i + kernel[k][0];
+				y = j + kernel[k][1];
+				index = (x*width+y)*4;
+				return getGrayValue(data[index+0],data[index+1],data[index+2])*kernel[k][2];
+			}
+			for(let i=1; i<height-1; i++){
+				for(let j=1; j<width-1; j++){
+					Gx = Gy = 0;
+					for(let k=0; k<6; k++){
+						Gx += calc(i,j,k,kernelX);
+						Gy += calc(i,j,k,kernelY);
+					}
+					index = (i*width+j)*4;
+					G = Math.abs(Gx)+Math.abs(Gy);
+					// G = Math.sqrt(Gx*Gx+Gy*Gy);
+					tmpData[index+0] = G;
+					tmpData[index+1] = G;
+					tmpData[index+2] = G;
+					tmpData[index+3] = data[index+3];
+				}
+			}
+			data.forEach((value,index,array)=>{
+				array[index] = tmpData[index];
+			});
+			return true;
+		},
 		blur:function(imageData,value = 3){             //均值模糊 -- 柔化
 			let {data,width,height} = imageData;
 			let blurRadius = value; //模糊半径
